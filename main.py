fd305b1eb1d1155b76b509b1b931eeef4fbad668
qing762 <threatedblade@outlook.com>
2023-10-27 18:24:41 +0800

    Removed unnecessary works
    .

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import platform
-import subprocess
 import time
 import validators
 import requests
@@ -10,13 +8,11 @@ website = input("Please enter your website domain: ")
 def ping(host):
     if validators.url(host) == True:
         r = requests.get(host)
-        print(r.status_code)
-        host = host.replace("https://", "").rstrip("/")
-        param = "-n" if platform.system().lower() == "windows" else "-c"
-        command = ["ping", param, "1", host]
-        return subprocess.call(command) == 0
+        return r.status_code
     else:
-        exit(f"Invalid URL '{host}': No scheme supplied. Perhaps you meant http://{host}?")
+        exit(
+            f"Invalid URL '{host}': No scheme supplied. Perhaps you meant http://{host}?"
+        )
 
 
 if __name__ == "__main__":
